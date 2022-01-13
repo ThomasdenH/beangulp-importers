ff6666f5957d9727354e3d23aacbedc839a71240
Thomas <denhollander.thomas@gmail.com>
2022-01-13 12:53:05 +0100

    First commit

---
+++ paypal/importer.py
@@ -0,0 +1,259 @@
+from enum import Enum
+from os import path
+from typing import Dict, Optional
+from beancount.core import data
+from beangulp.testing import main
+from beangulp.importers import csvbase
+import csv as pycsv
+from beancount.core import number
+from beancount.core.position import CostSpec
+import datetime
+
+english = {
+    "date": "Date",
+    "time": "Time",
+    "timezone": "TimeZone",
+    "name": "Name",
+    "typ": "Type",
+    "currency": "Currency",
+    "amount": "Net",
+    "balance": "Balance",
+    "subject": "Subject",
+    "bank_transfer": ["Bank Deposit to PP Account", "Algemene opname"], # < Translate
+    "general_currency_conversion": "General Currency Conversion",
+    "transaction_id": "Transaction ID",
+    "reference_transaction_id": "Reference Txn ID"
+}
+
+class MergeType(Enum):
+    BANK_TRANSFER = 0
+    CURRENCY_CONVERSION = 1
+
+pycsv.register_dialect("paypaldialect", delimiter=",")
+
+class CommaAmount(csvbase.Column):
+    def parse(self, value):
+        return number.D(value.replace(",", "."))
+
+
+class Importer(csvbase.Importer):
+    encoding = "utf-8-sig"
+    dialect = "paypaldialect"
+
+    lang = english
+
+    date = csvbase.Date(lang["date"], "%d/%m/%Y")
+    payee = csvbase.Column(lang["name"])
+    amount = CommaAmount(lang["amount"])
+    currency = csvbase.Column(lang["currency"])
+    balance_unreg = CommaAmount(lang["balance"])
+    narration = csvbase.Column(lang["subject"])
+    typ = csvbase.Column(lang["typ"])
+    time = csvbase.Column(lang["time"])
+    timezone = csvbase.Column(lang["timezone"])
+    transaction_id = csvbase.Column(lang["transaction_id"])
+    reference_transaction_id = csvbase.Column(lang["reference_transaction_id"])
+
+    def __init__(self, base_currency: str, account: str, bank_account: Optional[str]) -> None:
+        self.bank_account = bank_account
+        super().__init__(account, base_currency)
+
+    def filename(self, filepath):
+        return "paypal." + path.basename(filepath)
+
+    def identify(remap, file):
+        f = open(file)
+        is_correct = path.basename(f.name) == "Download.CSV"
+        f.close()
+        return is_correct
+
+    def extract(self, filepath, existing):
+        entries = super().extract(filepath, existing)
+        read = self.read(filepath)
+
+        iter = list(zip(entries, read))
+
+        new_entries = []
+
+        i = 0
+        while i < len(iter):
+            entry, row = iter[i]
+
+            has_expense = len(entry.postings) == 2
+
+            merges = []
+
+            has_bank_transfer = False
+            has_currency_conversion = False
+
+            while i + 1 < len(iter):
+                next_entry, next_row = iter[i + 1]
+                if next_row.reference_transaction_id == row.transaction_id:
+
+                    if next_row.typ in self.lang["bank_transfer"]:
+                        merges.append(MergeType.BANK_TRANSFER)
+                    elif next_row.typ == self.lang["general_currency_conversion"]:
+                        merges.append(MergeType.CURRENCY_CONVERSION)
+                    else:
+                        print('unsupported transaction type that references a previous transaction:', next_row.typ)
+
+                    entry = entry._replace(
+                        postings=entry.postings + next_entry.postings
+                    )
+                    i += 1
+
+            # If there bank transfer immediately after, add this to the posting.
+            # Later on, simplify to leave the account intact.
+            #if i + 1 < len(iter):
+            #    next_entry, next_row = iter[i + 1]
+            #    if (
+            #        next_row.typ in self.lang["bank_transfer"]
+            #        and next_row.reference_transaction_id == row.transaction_id
+            #    ):
+                    # Add the posting to the existing transaction.
+            #        entry = entry._replace(
+            #            postings=entry.postings + next_entry.postings
+            #        )
+            #        i += 1
+            #        has_bank_transfer = True
+
+            # Similarly for currency conversions
+            #if i + 2 < len(iter):
+            #    next_entry_1, next_row_1 = iter[i + 1]
+            #    next_entry_2, next_row_2 = iter[i + 2]
+            #    if (
+            #        next_row_1.typ == self.lang["general_currency_conversion"]
+            #        and next_row_2.typ == self.lang["general_currency_conversion"]
+            #        and next_row.reference_transaction_id == row.transaction_id
+            #    ):
+            #        postings = [next_entry_1.postings[0], next_entry_2.postings[0]]
+
+             #       if postings[0][1][1] != self.currency:
+              #          postings = [entries[i + 1].postings[0], entries[i].postings[0]]
+
+             #       cost_number_total = -postings[0][1][0]
+             #       cost_currency = postings[0][1][1]
+             #       postings[1] = postings[1]._replace(
+             #           cost=CostSpec(
+             #               None, cost_number_total, cost_currency, None, None, None
+             #           )
+             #       )
+
+              #      entry = entry._replace(postings=entry.postings + postings)
+
+                #    i += 2
+             #       has_currency_conversion = True
+
+            # Now we can have a single transaction with a lot of postings. Simplify.
+            #if has_bank_transfer and (has_currency_conversion or has_expense):
+            #    # First of all, try to see if added balance was removed immediately.
+            #    new_balance_posting_index = len(new_entry.postings) - 4
+            #    new_balance_remove_index = 0
+            #    if has_currency_conversion:
+            #        new_balance_remove_index = len(new_entry.postings) - 2
+            #    new_entry = new_entry._replace(
+            #        postings=[
+            #            posting
+            #            for (index, posting) in enumerate(new_entry.postings)
+            #            if not index
+            #            in [new_balance_posting_index, new_balance_remove_index]
+            #        ]
+            #    )
+
+            #if has_currency_conversion and has_expense:
+                # Now, move the cost spec to the actual expenses if the transaction is complete. The first and last entry should be removed, and the
+                # cost should be moved from the last to the second (which will become the first.)
+            #    first_index = 0
+            #    last_index = len(new_entry.postings) - 1
+            #    new_entry.postings[1] = new_entry.postings[1]._replace(
+            #        cost=new_entry.postings[last_index][2]
+             #   )
+             #   new_entry = new_entry._replace(
+             #       postings=[
+             #           posting
+             #           for (index, posting) in enumerate(new_entry.postings)
+             #           if not index in [first_index, last_index]
+             #       ]
+             #   )
+
+
+            # Add an expense posting from the first transaction.
+            paypal_account_posting = entry.postings[0]
+            first_entry = paypal_account_posting._replace(
+                # TODO: For clarity, use Income when money is coming in.
+                account = 'Expenses:UnknownAccount',
+                units = -paypal_account_posting.units
+            )
+            entry = entry._replace(
+                postings = [first_entry] + entry.postings[1:]
+            )
+
+            # If there was not a bank transfer, the money is coming from the paypal account. Add this posting (use the orinal posting).
+            if not MergeType.BANK_TRANSFER in merges:
+                entry = entry._replace(
+                    postings = entry.postings[:1] + [paypal_account_posting] + entry.postings[1:]
+                )
+
+            # The first transaction is the primary transaction. The others should be merged
+            while len(merges) > 0:
+                if merges[-1] == MergeType.BANK_TRANSFER:
+                    # The lowest posting is from a bank transfer. Invert the amount and add add a transfer posting.
+                    amount = -entry.postings[-1].units
+                    bank_transfer_posting = data.Posting(
+                        self.bank_account,
+                        amount,
+                        None,
+                        None,
+                        None,
+                        None,
+                    )
+                    entry = entry._replace(
+                            postings=entry.postings[:-1] + [bank_transfer_posting]
+                        )
+                    merges = merges[:-1]
+                elif merges[-1] == MergeType.CURRENCY_CONVERSION:
+                    # Assume each currency conversion comes in pairs.
+                    assert len(merges) >= 2 and merges[-2] == MergeType.CURRENCY_CONVERSION
+
+                    # The bottom two postings correspond to the conversion.
+
+                    # One currency gets subtracted, another added. Assume the second-last posting is the native currency.
+                    # Other situations are probably possible, but not currently handled.
+                    assert entry.postings[-2].units[1] == self.currency
+
+                    # The first entry is the expense. See if the units is identical, in which case we can simply add the
+                    # cost spec.
+                    assert entry.postings[0].units == entry.postings[-1].units
+
+                    # Add the cost spec
+                    entry.postings[0] = entry.postings[0]._replace(
+                        cost=CostSpec(
+                            None, -entry.postings[-2].units[0], self.currency, None, None, None
+                        )
+                    )
+
+                    # Remove the currency conversions.
+                    entry = entry._replace(
+                        postings = entry.postings[:-2]
+                    )
+
+                    merges = merges[:-2]
+
+            new_entries.append(entry)
+            i += 1
+
+        if len(iter) > 0:
+            entry, row = iter[-1]
+            if row.balance_unreg is not None:
+                date = row.date + datetime.timedelta(days=1)
+                units = data.Amount(row.balance_unreg, self.currency)
+                meta = data.new_metadata(filepath, entry.meta['lineno'])
+                new_entries.append(
+                    data.Balance(meta, date, self.account(filepath), units, None, None)
+                )
+
+        return new_entries
+
+
+if __name__ == "__main__":
+    main(Importer("Assets:Paypal", "EUR"))
---
+++ paypal/tests.py
@@ -0,0 +1,78 @@
+from paypal import importer
+
+import unittest
+
+from beancount.parser import cmptest, printer
+from beancount.utils.test_utils import docfile
+
+class TestPaypal(cmptest.TestCase):
+    @docfile
+    def test_basic_transaction(self, filename):
+        """\
+"Date","Time","TimeZone","Name","Type","Status","Currency","Gross","Fee","Net","From Email Address","To Email Address","Transaction ID","Reference Txn ID","Receipt ID","Balance","Subject"
+"10/12/2020","13:54:37","PST","Payee","Express Checkout Payment","Completed","EUR","-30,00","0,00","-30,00","email@address.com","anotheremail@address.com","253285736","","","-30,00","Transaction subject"
+        """
+        paypal_importer = importer.Importer("EUR", "Assets:Paypal", "Liabilities:DirectDebit")
+        entries = paypal_importer.extract(filename, [])
+        self.assertEqualEntries(entries,"""
+            2020-12-10 * "Payee" "Transaction subject"
+                Expenses:UnknownAccount   30.00 EUR
+                Assets:Paypal            -30.00 EUR
+
+            2020-12-11 balance Assets:Paypal                                   -30.00 EUR
+        """)
+
+    # Paypal files contain no header if there are no entries
+    @docfile
+    def test_empty_file(self, filename):
+        """
+        """
+        paypal_importer = importer.Importer("EUR", "Assets:Paypal", "Liabilities:DirectDebit")
+        entries = paypal_importer.extract(filename, [])
+        self.assertEqualEntries(entries, "")
+
+    @docfile
+    def test_direct_bank_transfer(self, filename):
+        """\
+"Date","Time","TimeZone","Name","Type","Status","Currency","Gross","Fee","Net","From Email Address","To Email Address","Transaction ID","Reference Txn ID","Receipt ID","Balance","Subject"
+"19/08/2020","03:00:00","PDT","Payee","Express Checkout Payment","Completed","EUR","-66,00","0,00","-66,00","email@address.com","anotheremail@address.com","transactionid23423","","","-66,00","Description"
+"19/08/2020","03:00:00","PDT","","Bank Deposit to PP Account ","Pending","EUR","66,00","0,00","66,00","","email@address.com","6324872369","transactionid23423","","0,00","Another description"
+        """
+        paypal_importer = importer.Importer("EUR", "Assets:Paypal", "Liabilities:DirectDebit")
+        entries = paypal_importer.extract(filename, [])
+        self.assertEqualEntries(entries, """
+            2020-08-19 * "Payee" "Description"
+                Expenses:UnknownAccount   66.00 EUR
+                Liabilities:DirectDebit  -66.00 EUR
+
+            2020-08-20 balance Assets:Paypal                                   0.00 EUR
+        """)
+
+    @docfile
+    def test_currency_conversion(self, filename):
+        """\
+"Date","Time","TimeZone","Name","Type","Status","Currency","Gross","Fee","Net","From Email Address","To Email Address","Transaction ID","Reference Txn ID","Receipt ID","Balance","Subject"
+"13/08/2019","07:00:00","PDT","Grocery Store","PreApproved Payment Bill User Payment","Completed","USD","-15,00","0,00","-15,00","email@address.com","anotheremail@address.com","transaction_id2343","63464334","","-15,00","An apple and an egg"
+"13/08/2019","07:00:00","PDT","","Bank Deposit to PP Account ","Pending","EUR","13,92","0,00","13,92","","email@address.com","id1","transaction_id2343","","13,92","An apple and an egg"
+"13/08/2019","07:00:00","PDT","","General Currency Conversion","Completed","EUR","-13,92","0,00","-13,92","email@address.com","","id2","transaction_id2343","","0,00","An apple and an egg"
+"13/08/2019","07:00:00","PDT","","General Currency Conversion","Completed","USD","15,00","0,00","15,00","","email@address.com","id3","transaction_id2343","","0,00","An apple and an egg"
+        """
+        paypal_importer = importer.Importer("EUR", "Assets:Paypal", "Liabilities:DirectDebit")
+        entries = paypal_importer.extract(filename, [])
+
+        # For some reason comparing directly doesn't work
+        actual = ''
+        for entry in entries:
+            actual = actual + printer.format_entry(entry)
+
+        expected = """\
+2019-08-13 * "Grocery Store" "An apple and an egg"
+  Expenses:UnknownAccount   15.00 USD {# 13.92 EUR}
+  Liabilities:DirectDebit  -13.92 EUR
+2019-08-14 balance Assets:Paypal                                   0.00 EUR
+"""
+
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
