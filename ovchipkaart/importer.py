3193ca309dcbbddbc652b3cda7c3e8ca34fc0e4c
Thomas <denhollander.thomas@gmail.com>
2022-01-13 14:01:11 +0100

    Add ov chipkaart importer

---
+++ ovchipkaart/importer.py
@@ -0,0 +1,97 @@
+from os import path
+from turtle import pos
+from typing import Optional
+from beangulp.testing import main
+from beangulp.importers import csvbase
+import csv as pycsv
+from beancount.core import number, data
+
+pycsv.register_dialect("ovchipkaartdialect", delimiter=";")
+
+
+class CommaAmount(csvbase.Column):
+    def parse(self, value):
+        return number.D(value.replace(",", "."))
+
+
+class Importer(csvbase.Importer):
+    encoding = "utf8"
+    names = True
+    dialect = "ovchipkaartdialect"
+
+    date = csvbase.Date("Datum", "%d-%m-%Y")
+    amount = CommaAmount("Bedrag")
+    narration = csvbase.Column("Transactie")
+    starting_location = csvbase.Column("Vertrek")
+    destination = csvbase.Column("Bestemming")
+    product = csvbase.Column("Product")
+
+    #! - account: The card account
+    #! - bank_account: The bank account that money is subtracted from. Tip: Can be Liabilities:OvIncasso
+    #! - currency: The currency.
+    def __init__(
+        self, account: str, bank_account: Optional[str], currency: str = "EUR"
+    ) -> None:
+        self.bank_account = bank_account
+        super().__init__(account, currency)
+
+    def filename(self, filepath):
+        return "ovchipkaart." + path.basename(filepath)
+
+    def extract(self, filepath, existing):
+        return [
+            entry
+            for entry in super().extract(filepath, existing)
+            if not entry.postings[0].units.number.is_zero()
+        ]
+
+    def identify(remap, file):
+        header = '"Datum";"Check-in";"Vertrek";"Check-uit";"Bestemming";"Bedrag";"Transactie";"Klasse";"Product";"Opmerkingen";"Naam";"Kaartnummer"'
+        with open(file) as fd:
+            head = fd.read(len(header))
+        return head.startswith(header)
+
+    def finalize(self, transaction, row):
+        if row.narration == "Saldo automatisch opgeladen":
+            topup_at = row.destination  # Contains something like 'bij NS'
+            transaction = transaction._replace(
+                narration=transaction.narration + " " + topup_at
+            )
+
+            # We know what the other posting should be if it was supplied. Add it here.
+            if not self.bank_account == None:
+                transaction = transaction._replace(
+                    postings=transaction.postings
+                    + [
+                        data.Posting(
+                            self.bank_account,
+                            -transaction.postings[0].units,
+                            None,
+                            None,
+                            None,
+                            None,
+                        )
+                    ]
+                )
+        else:
+            if (
+                row.starting_location == None
+                or row.destination == None
+                or row.product == None
+            ):
+                transaction = transaction._replace(narration=None)
+            else:
+                transaction = transaction._replace(
+                    narration=row.starting_location
+                    + " - "
+                    + row.destination
+                    + " - "
+                    + row.product
+                )
+
+            amount = transaction.postings[0].units
+            postings = transaction.postings
+            postings[0] = postings[0]._replace(units=-amount)
+            transaction = transaction._replace(postings=postings)
+
+        return transaction
---
+++ ovchipkaart/tests.py
@@ -0,0 +1,43 @@
+from ovchipkaart import importer
+
+from beancount.parser import cmptest
+from beancount.utils.test_utils import docfile
+
+
+class TestPaypal(cmptest.TestCase):
+    @docfile
+    def test_topup_transaction(self, filename: str) -> None:
+        """\
+"Datum";"Check-in";"Vertrek";"Check-uit";"Bestemming";"Bedrag";"Transactie";"Klasse";"Product";"Opmerkingen";"Naam";"Kaartnummer"
+"29-03-2021";"16:00";"";"";"bij NS";"10,00";"Saldo automatisch opgeladen";"";"";"";"Card owner name";"1234 5678 9012 3456"
+        """
+        ovchipkaart_importer = importer.Importer(
+            "Assets:Vervoer:OvChipkaarttegoed", "Liabilities:OvIncasso", "EUR"
+        )
+        entries = ovchipkaart_importer.extract(filename, [])
+        self.assertEqualEntries(
+            entries,
+            """        
+            2021-03-29 * "Saldo automatisch opgeladen bij NS"
+                Assets:Vervoer:OvChipkaarttegoed     10.00 EUR
+                Liabilities:OvIncasso               -10.00 EUR
+        """,
+        )
+
+    @docfile
+    def test_travel_transaction(self, filename: str) -> None:
+        """\
+"Datum";"Check-in";"Vertrek";"Check-uit";"Bestemming";"Bedrag";"Transactie";"Klasse";"Product";"Opmerkingen";"Naam";"Kaartnummer"
+"29-03-2021";"";"Eindhoven Centraal";"11:49";"Nijmegen";"8,88";"Check-uit";"";"Transactiebeschrijving";"";"Card owner name";"1234 5678 9012 3456"
+        """
+        ovchipkaart_importer = importer.Importer(
+            "Assets:Vervoer:OvChipkaarttegoed", "Liabilities:OvIncasso", "EUR"
+        )
+        entries = ovchipkaart_importer.extract(filename, [])
+        self.assertEqualEntries(
+            entries,
+            """
+            2021-03-29 * "Eindhoven Centraal - Nijmegen - Transactiebeschrijving"
+                Assets:Vervoer:OvChipkaarttegoed  -8.88 EUR
+        """,
+        )
